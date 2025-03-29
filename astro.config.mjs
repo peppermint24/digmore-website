a35aaf8620992fa0a6ec4064fca8f5df67a92913
medievalstone <49617014+MedievalStone@users.noreply.github.com>
2025-03-28 20:28:01 -0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  build: {
+    assets: '_astro',
+    inlineStylesheets: 'auto'
+  },
+  compressHTML: true,
+  outDir: './docs'
+});
